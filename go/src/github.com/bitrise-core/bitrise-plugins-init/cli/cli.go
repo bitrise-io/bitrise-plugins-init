83141c5263f4cee72dd17aeaa8f8089f32619eaf
Krisztián Gödrei <godrekrisztian@gmil.com>
2016-03-17 17:35:23 +0100

    plugin init

---
+++ go/src/github.com/bitrise-core/bitrise-plugins-init/cli/cli.go
@@ -0,0 +1,104 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/bitrise-core/bitrise-plugins-init/version"
+	"github.com/codegangsta/cli"
+)
+
+//=======================================
+// Variables
+//=======================================
+
+const (
+	pluginInputPayloadKey        = "BITRISE_PLUGIN_INPUT_PAYLOAD"
+	pluginInputBitriseVersionKey = "BITRISE_PLUGIN_INPUT_BITRISE_VERSION"
+	pluginInputTriggerEventKey   = "BITRISE_PLUGIN_INPUT_TRIGGER"
+	pluginInputPluginModeKey     = "BITRISE_PLUGIN_INPUT_PLUGIN_MODE"
+	pluginInputDataDirKey        = "BITRISE_PLUGIN_INPUT_DATA_DIR"
+
+	bitrisePluginOutputEnvKey = "BITRISE_PLUGIN_OUTPUT"
+)
+
+//=======================================
+// Functions
+//=======================================
+
+func printVersion(c *cli.Context) {
+	fmt.Fprintf(c.App.Writer, "%v\n", c.App.Version)
+}
+
+func before(c *cli.Context) error {
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp:   true,
+		ForceColors:     true,
+		TimestampFormat: "15:04:05",
+	})
+
+	// Log level
+	// If log level defined - use it
+	logLevelStr := c.String("loglevel")
+	if logLevelStr == "" {
+		logLevelStr = "info"
+	}
+
+	level, err := log.ParseLevel(logLevelStr)
+	if err != nil {
+		return err
+	}
+	log.SetLevel(level)
+
+	bitriseVersion := os.Getenv(pluginInputBitriseVersionKey)
+
+	log.Debug("")
+	log.Debugf("pluginInputBitriseVersion: %s", bitriseVersion)
+
+	triggerEvent := os.Getenv(pluginInputTriggerEventKey)
+
+	log.Debug("")
+	log.Debugf("pluginInputTriggerEvent: %s", triggerEvent)
+
+	dataDir := os.Getenv(pluginInputDataDirKey)
+
+	log.Debug("")
+	log.Debugf("pluginInputDataDir: %s", dataDir)
+
+	return nil
+}
+
+//=======================================
+// Main
+//=======================================
+
+// Run ...
+func Run() {
+	// Parse cl
+	cli.VersionPrinter = printVersion
+
+	app := cli.NewApp()
+
+	app.Name = path.Base(os.Args[0])
+	app.Usage = "Bitrise Init plugin"
+	app.Version = version.VERSION
+
+	app.Author = ""
+	app.Email = ""
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "loglevel, l",
+			Usage:  "Log level (options: debug, info, warn, error, fatal, panic).",
+			EnvVar: "LOGLEVEL",
+		},
+	}
+	app.Before = before
+	app.Commands = []cli.Command{}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal("Finished with Error:", err)
+	}
+}
