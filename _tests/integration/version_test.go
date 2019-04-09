85078ba46704906a859681662d5c0865a6402305
Papik Tamas <trapacska@gmail.com>
2019-04-09 10:55:36 +0200

    initial clean state

--- _tests/integration/version_test.go
+++ _tests/integration/version_test.go
@@ -3,7 +3,7 @@ package integration
 import (
 	"testing"
 
-	"github.com/bitrise-core/bitrise-plugins-init/version"
+	"github.com/bitrise-io/bitrise-plugins-init/version"
 	"github.com/bitrise-io/go-utils/command"
 	"github.com/bitrise-io/go-utils/pathutil"
 	"github.com/stretchr/testify/require"
