7a168b281f31c73ede2f76976f0ee8ac8613a1c0
Laszlo Szucs <laszlo.szucs@bitrise.io>
2020-01-13 18:21:21 +0100

    add test case: secrets file already ignored

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -63,4 +63,22 @@ func Test_GitignoreTest(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, "node_modules\n.bitrise.secrets.yml", string(contents))
 	}
+
+	t.Log("do not add .bitrise.secrets.yml to .gitignore if it's already there")
+	{
+		tmpDir, err := pathutil.NormalizedOSTempDirPath("")
+		require.NoError(t, err)
+
+		gitignorePath := path.Join(tmpDir, ".gitignore")
+		content := []byte(".bitrise.secrets.yml")
+		err = ioutil.WriteFile(gitignorePath, content, 0644)
+		require.NoError(t, err)
+
+		err = gitignore(ignorePattern, gitignorePath)
+		require.NoError(t, err)
+
+		contents, err := ioutil.ReadFile(gitignorePath)
+		require.NoError(t, err)
+		require.Equal(t, ".bitrise.secrets.yml", string(contents))
+	}
 }
